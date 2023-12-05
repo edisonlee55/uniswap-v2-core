4a80ce327aadf309b6fa60cb02685a5087c94341
Edison Lee <edisonlee@edisonlee55.com>
2023-12-05 09:45:34 +0800

    refactor: rename to owner address

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,22 +4,22 @@ const UniswapV2Factory = artifacts.require("UniswapV2Factory");
 const FeeToSetter = artifacts.require("FeeToSetter");
 const FeeTo = artifacts.require("FeeTo");
 
-const DaoTimelockAddress = process.env.DAO_TIMELOCK_ADDRESS; // DAOTimelock
+const OwnerAddress = process.env.OWNER_ADDRESS;
 
 module.exports = async function (deployer, network, accounts) {
     console.log("Deploying to network: " + network);
     console.log("Deployer address: " + accounts[0]);
 
-    console.log(DaoTimelockAddress);
+    console.log("Owner address: " + OwnerAddress);
 
     await deployer.deploy(UniswapV2Factory, accounts[0]); // _feeToSetter
 
-    await deployer.deploy(FeeTo, DaoTimelockAddress); // owner_
+    await deployer.deploy(FeeTo, OwnerAddress); // owner_
 
     await deployer.deploy(FeeToSetter,
         UniswapV2Factory.address, // factory_
         1701336600, // vestingEnd_ (must be in the future)
-        DaoTimelockAddress, // owner_
+        OwnerAddress, // owner_
         FeeTo.address, // feeTo_
     );
 
